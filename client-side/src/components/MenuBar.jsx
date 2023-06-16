b3f638da08abfbb91a62b176f065d5f746b7ffa8
william <laurencius.william@gmail.com>
2023-06-16 16:57:52 +0700

    feat :layouting landing page

---
+++ client-side/src/components/MenuBar.jsx
@@ -0,0 +1,38 @@
+export default function  MenuBar(){
+    return(
+        <div>
+        <div className="w-full">
+        <ul className="menu menu-vertical lg:menu-horizontal bg-blue-900  text-white flex w-full">
+        <li><a>menitNews</a></li>
+        <li><a>menitFinance</a></li>
+        <li><a>menitHot</a></li>
+        <li><a>menitInet</a></li>
+        <li><a>menitSport</a></li>
+        <li><a>menitOto</a></li>
+        <li><a>menitTravel</a></li>
+        <li><a>menitFood</a></li>
+        <li><a>menitHealth</a></li>
+        <li><a>Wolipop</a></li>
+        <li><a>30Menit</a></li>
+        </ul>
+        </div>
+
+        <div className="w-full">
+        <ul className="menu menu-vertical lg:menu-horizontal bg-slate-600  text-white flex w-full">
+        <li><a>Live Tv</a></li>
+        <li><a>Ads Smart</a></li>
+        <li><a>MenitX</a></li>
+        <li><a>Sepak Bola</a></li>
+        <li><a>Hikmah</a></li>
+        <li><a>Edukasi</a></li>
+        <li><a>Pemilu 2070</a></li>
+        <li><a>Emeron Hijab Hunt</a></li>
+        <li><a>Badminton</a></li>
+        <li><a>Bola Basket</a></li>
+        <li><a>Bola Voli</a></li>
+        </ul>
+        </div>
+        </div>
+  
+    )
+}
